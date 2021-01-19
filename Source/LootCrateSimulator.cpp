23e58c89f402bd3854747bef2335472ad7d8859c
51VR\yejing <wangyejing@51hitech.com>
2021-01-19 10:53:29 +0800

    Added a test json

--- Source/LootCrateSimulator.cpp
+++ Source/LootCrateSimulator.cpp
@@ -13,26 +13,25 @@ bool loadString(
     nlohmann::json& jsonNode,
     const std::string& content)
 {
-    try
-    {
+    try {
         jsonNode = nlohmann::json::parse(content);
         return true;
     }
-    catch (nlohmann::json::parse_error& e)
-    {
+    catch (nlohmann::json::parse_error& e) {
         std::string s;
         size_t pos1 = (e.byte - 50 < 0 ? 0 : e.byte - 50);
-        if (e.byte > 1)
+        if (e.byte > 1) {
             s += content.substr(pos1, e.byte - 1 - pos1);
+        }
         s += " [[[ " + content.substr(e.byte - 1, 1) + " ]]] ";
-        if (e.byte < content.length())
+        if (e.byte < content.length()) {
             s += content.substr(e.byte);
+        }
         printf("Failed to parse json string near the [[[]]] marks: [%s]", s.c_str());
         printf("Error %d: %s", e.id, e.what());
         return false;
     }
-    catch (...)
-    {
+    catch (...) {
         printf("Unknown errors when parsing json string [%s]", content.c_str());
         return false;
     }
